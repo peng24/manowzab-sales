9c650fc74dbd24cf4635b3aa75cf6e979821d193
peng24 <peng24@gmail.com>
2026-02-03 16:20:36 +0700

    feat: Add Vue Router configuration with authentication guards and bump package version.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
 import MainLayout from "../layouts/MainLayout.vue";
@@ -54,7 +54,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory("/manowzab-sales/"),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
